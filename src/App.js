52a7b2733609b9d16b60c48f3ffe2f54d8904a53
G <igauravsehrawat@gmail.com>
2018-10-03 20:21:59 +0530

    Frontend setup as create-react-app with antd design libary.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Button type="primary">Button</Button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
