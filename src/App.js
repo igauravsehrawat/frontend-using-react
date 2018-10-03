11315606e22dbdc10f529d19e0a6dbbce582ef90
G <igauravsehrawat@gmail.com>
2018-10-03 20:38:01 +0530

    Optimized app size with by using `babel-plugin-import`. Used `react-app-rewired` to avoid ejecting the create-react-app.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Button from 'antd/lib/button';
+// import Button from 'antd/lib/button';
+import { Button } from 'antd';
 import './App.css';
 
 class App extends Component {
