b8b98abd098e28b4ba68cda0d747438a0ee16e44
G <igauravsehrawat@gmail.com>
2018-10-04 20:31:58 +0530

    --add: hot module reloading enabled.
    Easy development.

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
@@ -9,5 +10,6 @@ module.exports = function override(config, env) {
     }],
     config
   )
+  config = rewireReactHotLoader(config, env);
   return config;
 }
