d0489a392d093e440c3e36d81ba01cfef4564b88
G <igauravsehrawat@gmail.com>
2018-10-07 17:58:37 +0530

    --fix: `AppContainer` should be above `App` for right hot reloading.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-import { AppContainer } from 'react-hot-loader';
 
 const render = () => {
   ReactDOM.render(
     <AppContainer>
       <App />
     </AppContainer>,
-    document.getElementById('root')
+    document.getElementById('root'),
   );
-}
+};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
