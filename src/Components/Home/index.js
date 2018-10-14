79150b82fedb3e733a6b84e22ed8f7f9ed12aac1
G <igauravsehrawat@gmail.com>
2018-10-14 14:21:28 +0530

    --update: Changed Home to stateless component.

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './index.scss';
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="fade-in-left wave-full-parent-height wave-flex wave-flex-center home">
-        <p>Welcome to payroll system</p>
-      </div>
-    );
-  }
+function Home() {
+  return (
+    <div className="fade-in-left wave-full-parent-height wave-flex wave-flex-center home">
+      <p>Welcome to payroll system</p>
+    </div>
+  );
 }
 
 export default Home;
