7cdacb859a7f6277cb4995fa3e86beb1711b661c
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-03-19 00:02:01 -0700

    Collapsible menu integrated
    When website is shrunk or display on mobile is less than 500px in width, the navbar shrinks to collapsible menu.

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,7 +3,9 @@ import { Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const { Header } = Layout;
+const { Header, Sider } = Layout;
+// Window width of 500px
+const mobileWidthBreak = 500;
 
 class NavBar extends Component {
   constructor(props) {
@@ -11,6 +13,7 @@ class NavBar extends Component {
     const { selectedKeys } = this.props;
     this.state = {
       selectedKeys,
+      windowWidth: window.innerWidth,
     };
     this.setSelectedKeys = this.setSelectedKeys.bind(this);
   }
@@ -21,21 +24,61 @@ class NavBar extends Component {
     });
   }
 
+  handleResize = () => {
+    this.setState({ windowWidth: window.innerWidth });
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
   render() {
     const { selectedKeys } = this.state;
+    const { windowWidth } = this.state;
+
     return (
-      <Header>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          selectedKeys={selectedKeys}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="/" onClick={() => this.setSelectedKeys(['/'])}><Link to="/">Home</Link></Menu.Item>
-          <Menu.Item key="/upload-worklog-report" onClick={() => this.setSelectedKeys(['/upload-worklog-report'])}><Link to="/upload-worklog-report">Upload Report</Link></Menu.Item>
-          <Menu.Item key="/view-payroll-report" onClick={() => this.setSelectedKeys(['/view-payroll-report'])}><Link to="/view-payroll-report">View Payroll Report</Link></Menu.Item>
-        </Menu>
-      </Header>
+      <>
+        { windowWidth > mobileWidthBreak ?
+          <Header>
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              selectedKeys={selectedKeys}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="/" onClick={() => this.setSelectedKeys(['/'])}><Link to="/">Home</Link></Menu.Item>
+              <Menu.Item key="/upload-worklog-report" onClick={() => this.setSelectedKeys(['/upload-worklog-report'])}><Link to="/upload-worklog-report">Upload Report</Link></Menu.Item>
+              <Menu.Item key="/view-payroll-report" onClick={() => this.setSelectedKeys(['/view-payroll-report'])}><Link to="/view-payroll-report">View Payroll Report</Link></Menu.Item>
+            </Menu>
+          </Header>
+          :
+          <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={(broken) => {
+              console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+              console.log(collapsed, type);
+            }}
+          >
+            <Menu
+              theme="dark"
+              mode="inline"
+              selectedKeys={selectedKeys}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="/" onClick={() => this.setSelectedKeys(['/'])}><Link to="/">Home</Link></Menu.Item>
+              <Menu.Item key="/upload-worklog-report" onClick={() => this.setSelectedKeys(['/upload-worklog-report'])}><Link to="/upload-worklog-report">Upload Report</Link></Menu.Item>
+              <Menu.Item key="/view-payroll-report" onClick={() => this.setSelectedKeys(['/view-payroll-report'])}><Link to="/view-payroll-report">View Payroll Report</Link></Menu.Item>
+            </Menu>
+          </Sider>
+        }
+      </>
     );
   }
 }
